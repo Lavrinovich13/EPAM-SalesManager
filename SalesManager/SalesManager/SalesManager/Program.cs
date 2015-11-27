7be90c4be3df7f5ff1f653ef56648a119c58507c
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-27 22:18:52 +0300

    added console client

--- SalesManager/SalesManager/SalesManager/Program.cs
+++ SalesManager/SalesManager/SalesManager/Program.cs
@@ -3,13 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using BL.Scheduler;
+using System.Threading;
+using BL;
+using BL.Parser;
+using BL.Reader;
+using System.Configuration;
 
-namespace SalesManager
+namespace ConsoleSalesManager
 {
     class Program
     {
+        protected static TaskFactory _taskFactory;
+        protected static FileHandler _fileHandler;
+
         static void Main(string[] args)
         {
+            FileSystemWatcher watcher = new FileSystemWatcher();
+
+            watcher.Path = ConfigurationManager.AppSettings["FolderPath"];
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+               | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.csv";
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+
+            var taskScheduler = new LimitedTaskScheduler(Int32.Parse(ConfigurationManager.AppSettings["MaxDatabaseConnections"]));
+            _taskFactory = new TaskFactory(taskScheduler);
+
+            _fileHandler = new FileHandler();
+
+            Console.Read();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            _taskFactory.StartNew(() =>
+            {
+                _fileHandler.Handle(ConfigurationManager.AppSettings["FolderPath"], e.Name);
+            });
         }
     }
 }
